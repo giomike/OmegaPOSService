36ca331f5efcd67a35393f83537a2333635e06a7
wdpmaster <249230266@qq.com>
2025-12-15 10:14:58 +0800

    脚本优化

--- SQL/SYNC/MPos_Sync_SaveSku.sql
+++ SQL/SYNC/MPos_Sync_SaveSku.sql
@@ -1,47 +1,46 @@
 DROP PROCEDURE MPos_Sync_SaveSku
 GO
-CREATE PROCEDURE MPos_Sync_SaveSku @barcode VARCHAR(50), @styleID VARCHAR(15), @colorID CHAR(3), @sizeID CHAR(3)
+CREATE PROCEDURE MPos_Sync_SaveSku
+   @barcode varchar(50),
+   @styleID varchar(15),
+   @colorID char(3),
+   @sizeID  char(3)
 AS
-DECLARE @rtn TABLE(returnID INT, returnMessage VARCHAR(256))
+   SET NOCOUNT ON;
 
-IF NOT EXISTS(SELECT * FROM mfskun a WHERE a.skskun = @barcode)
-	BEGIN
-		INSERT dbo.mfskun
-		(
-		    skstyl,
-		    skcolr,
-		    sksize,
-		    skskun
-		)
-		VALUES
-		(   @styleID, -- skstyl - char(15)
-		    @colorID, -- skcolr - char(3)
-		    @sizeID, -- sksize - char(3)
-		    @barcode    -- skskun - char(21)
-		    )
-		
-		INSERT @rtn
-		(
-			returnID,
-			returnMessage
-		)
-		VALUES
-		(   1,   -- returnID - int
-			'成功' -- returnMessage - varchar(256)
-			);
-	END
-		BEGIN
-		INSERT @rtn
-		(
-			returnID,
-			returnMessage
-		)
-		VALUES
-		(   0,   -- returnID - int
-			'已经存在' -- returnMessage - varchar(256)
-			);
+   DECLARE @rtn TABLE
+   (
+       returnID      int,
+       returnMessage varchar( 256 )
+   )
 
-        END
-        
+   IF NOT EXISTS( SELECT *
+                  FROM   mfskun a
+                  WHERE  a.skskun = @barcode )
+      BEGIN
+         INSERT dbo.mfskun
+                (skstyl,skcolr,sksize,skskun)
+         VALUES ( @styleID,-- skstyl - char(15)
+                  @colorID,-- skcolr - char(3)
+                  @sizeID,-- sksize - char(3)
+                  @barcode -- skskun - char(21)
+         )
 
-	
+         INSERT @rtn
+                (returnID,returnMessage)
+         VALUES ( 1,-- returnID - int
+                  '成功' -- returnMessage - varchar(256)
+         );
+      END
+   ELSE
+      BEGIN
+         INSERT @rtn
+                (returnID,returnMessage)
+         VALUES ( 0,-- returnID - int
+                  '已经存在' -- returnMessage - varchar(256)
+         );
+      END
+
+   SELECT *
+   FROM   @rtn 
+ 
